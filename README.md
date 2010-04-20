aac4ca0e15ec5ad194c996e96a9044149f45054d
Clinton Sheppard <csheppard@mvbalaw.com>
2010-04-20 13:21:14 -0500

    initial checkin

---
+++ README.md
@@ -0,0 +1,20 @@
+## Assembly Info
+
+This commandline program can be used to output information about a .Net DLL/EXE.
+
+## Sample Output
+
+    Name          : AssemblyInfo
+    Version       : 1.0.0.0
+    Signed        : False
+    Culture       :
+    Architecture  : AnyCPU
+
+## License		
+
+[MIT License][mitlicense]
+
+This project is part of [MVBA Law Commons][mvbalawcommons].
+
+[mvbalawcommons]: http://code.google.com/p/mvbalaw-commons/
+[mitlicense]: http://www.opensource.org/licenses/mit-license.php
---
+++ src/AssemblyInfo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyInfo
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Usage: AssemblyInfo <assembly>\nexample:\n\n\tAssemblyInfo foo.exe\n");
+				return;
+			}
+
+			var assembly = Assembly.LoadFrom(args[0]);
+			var name = assembly.GetName();
+			string description = GetAttribute<AssemblyDescriptionAttribute>(assembly).Description;
+			var publicKey = name.GetPublicKey();
+
+
+			if (!String.IsNullOrEmpty(description))
+			{
+				Write("Description", description);
+			}
+			Write("Name", name.Name);
+			Write("Version", name.Version);
+			bool signed = publicKey != null && publicKey.Length > 0;
+			Write("Signed", signed);
+			Write("Culture", name.CultureInfo);
+			Write("Architecture", name.ProcessorArchitecture == ProcessorArchitecture.MSIL
+			                      	? "AnyCPU"
+			                      	: name.ProcessorArchitecture.ToString());
+		}
+
+		private static void Write<T>(string key, T value)
+		{
+			Console.WriteLine(key.PadRight(13) + " : " + value);
+		}
+
+		private static T GetAttribute<T>(Assembly assembly)
+		{
+			return (T) assembly.GetCustomAttributes(typeof (T), false)[0];
+		}
+	}
+}
